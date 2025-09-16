6ebf5cf27588155bd9ca663ba6b35df35b43800a
Vyirps <11820072@usc.edu.ph>
2025-09-16 08:51:36 +0800

    Var 4

---
+++ Variation 4.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#define LENGTH 10
+
+typedef struct{
+    int * elemPtr;
+    int count;
+    int max;
+}List, *EPtr;
+
+void initialize(List * L);
+void insertPos(List L, int data, int position);
+void deletePos(List L, int position);
+int locate(List L, int data);
+int retrieve(List L, int position);
+void insertSorted(List L, int data);
+void display(List L);
+void resize(List L);
+void makeNULL(List * L);
+
+
+
+int main(){
+
+}
+
+
+void initialize(List * L)
+
+
