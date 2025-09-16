3c05ab1b820b3ce1afc0409e6c7a9733a1cc50e0
Vyirps <11820072@usc.edu.ph>
2025-09-16 11:20:16 +0800

    v

--- Variation 4.c
+++ Variation 4.c
@@ -1,15 +1,16 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 #define LENGTH 10
 
-typedef struct{
-    int * elemPtr;
+typedef struct
+{
+    int *elemPtr;
     int count;
     int max;
-}List, *EPtr;
+} List, *EPtr;
 
-void initialize(List * L);
+void initialize(List *L);
 void insertPos(List L, int data, int position);
 void deletePos(List L, int position);
 int locate(List L, int data);
@@ -17,21 +18,76 @@ int retrieve(List L, int position);
 void insertSorted(List L, int data);
 void display(List L);
 void resize(List L);
-void makeNULL(List * L);
+void makeNULL(List *L);
 
-
-
-int main(){
+int main()
+{
 
     EPtr L;
-
+    initialize(L);
 }
 
-
-void initialize(List * L){
+void initialize(List *L)
+{
     L->elemPtr = malloc(LENGTH * sizeof(int));
     L->count = 0;
     L->max = LENGTH;
 }
 
+void insertPos(List L, int data, int position)
+{
 
+
+
+
+}
+
+void deletePos(List L, int position)
+{
+
+
+
+
+}
+int locate(List L, int data)
+{
+
+
+
+
+}
+
+int retrieve(List L, int position)
+{
+
+
+
+
+}
+
+void insertSorted(List L, int data)
+{
+
+
+
+    
+}
+
+void display(List L)
+{
+    printf("\nDisplaying current list");
+    for (int i = 0; i <= L.count; i++)
+    {
+        printf("\nIndex %d: Data %d", i, L.elemPtr[i]);
+    }
+}
+void resize(List L)
+{
+    L->max *= 2;
+    L = realloc(L->elemPtr, L->max * sizeof(int));
+}
+
+void makeNULL(List *L)
+{
+    free(L->elemPtr);
+}
