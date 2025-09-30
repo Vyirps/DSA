e5708bf02b1176fe347e37b498fceee04435a546
Vyirps <11820072@usc.edu.ph>
2025-09-30 13:12:58 +0800

    w

---
+++ 7 - Bit Vector/Var 1 Bitmask.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+void initialize(unsigned char * set);
+void insert(unsigned char * set, int element);
+void delete(unsigned char * set, int element);
+bool find(unsigned char * set, int element);
+unsigned char Union(unsigned char A, unsigned char B);
+unsigned char intersection(unsigned char A, unsigned char B);
+unsigned char difference(unsigned char A, unsigned char B);
+void display(unsigned char set);
+
+int main(){
+
+}
+
+void initialize(unsigned char * set){
+
+
+
+}
+
+void insert(unsigned char * set, int element){
+
+
+}
+
+void delete(unsigned char * set, int element){
+
+
+}
+
+bool find(unsigned char * set, int element){
+
+
+
+}
+
+unsigned char Union(unsigned char A, unsigned char B){
+
+
+}
+
+unsigned char intersection(unsigned char A, unsigned char B){
+
+
+}
+unsigned char difference(unsigned char A, unsigned char B){
+
+
+    
+}
+void display(unsigned char set);
