fdbff24455216855647f03b86920e3260b98afe8
Vyirps <11820072@usc.edu.ph>
2025-09-30 14:43:58 +0800

    Bitfields

---
+++ 7 - Bit Vector/Var 2 Bitfields.c
@@ -0,0 +1,62 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+void initialize(Set * set);
+void insert (Set * set, int element);
+void delete(Set * set,int element);
+bool find(Set set, int element);
+Set Union(Set A, Set B);
+Set intersections(Set A, Set B);
+Set difference(Set A, Set B);
+void display(unsigned char set);
+
+
+
+int main(){
+
+}
+
+
+void initialize(Set * set){
+
+
+
+}
+
+
+void insert (Set * set, int element){
+
+
+
+}
+void delete(Set * set,int element){
+
+
+
+}
+bool find(Set set, int element){
+
+
+
+}
+Set Union(Set A, Set B){
+
+
+
+}
+Set intersections(Set A, Set B){
+
+
+
+}
+Set difference(Set A, Set B){
+
+
+
+}
+
+
+void display(unsigned char set){
+
+
+}
