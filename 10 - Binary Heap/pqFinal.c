4e39389064d7febc824844f3bb4d2154c08251f0
Vyirps <11820027@usc.edu.ph>
2025-12-10 21:18:10 +0800

    pq

---
+++ 10 - Binary Heap/pqFinal.c
@@ -0,0 +1,125 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+#define MAX 10
+
+
+typedef struct{
+    int pqArr[MAX]; 
+    int size; 
+    int capacity; 
+}PriorityQueue; 
+
+void initPQ(PriorityQueue * pq);
+void insert(PriorityQueue * pq, int val);
+int extractRoot(PriorityQueue * pq); 
+void heapify(PriorityQueue * pq, int size, int index); 
+void heapsort(PriorityQueue * pq); 
+void swap(int * a, int * b); 
+void printHeap(PriorityQueue * pq); 
+
+
+int main(){
+    PriorityQueue * pq = malloc(sizeof(PriorityQueue)); 
+    initPQ(pq);
+
+    insert(pq, 1); 
+    insert(pq, 6); 
+    insert(pq, 3); 
+    insert(pq, 9); 
+    insert(pq, 19); 
+    printHeap(pq); 
+    int val = extractRoot(pq); 
+    printf("hello %d", val);
+        printHeap(pq);
+    heapsort(pq); 
+    printHeap(pq); 
+
+}
+
+
+
+
+
+void initPQ(PriorityQueue * pq){
+    pq->size = 0; 
+    pq->capacity = MAX; 
+    printf("----- Initializing Heap -----\n\n"); 
+}
+void insert(PriorityQueue * pq, int val){
+
+    if(pq->size != MAX){
+            pq->pqArr[pq->size++] = val; 
+            for(int i = pq->size/2-1; i>=0 ; i--){
+                heapify(pq, pq->size, i); 
+            }
+    
+            printf("Inserted value %d successfully\n", val);
+        }
+}
+int extractRoot(PriorityQueue * pq){
+    
+    int val; 
+    if(pq->size != 0 ){
+        val = pq->pqArr[0]; 
+        swap(&pq->pqArr[0], &pq->pqArr[--pq->size]); 
+        heapify(pq, pq->size, 0); 
+        printf("Deleted value %d successfully\n", val); 
+    }
+
+    return val; 
+    
+}
+void heapify(PriorityQueue * pq, int size, int index){
+    int largest = index; 
+    int left = (index * 2) + 1; 
+    int right = (index * 2) + 2;
+
+    if(left < size && pq->pqArr[left] > pq->pqArr[largest]){
+        largest = left; 
+    }
+
+    if(right < size && pq->pqArr[right] > pq->pqArr[largest]){
+        largest = right; 
+    }
+
+
+    if(largest != index){
+        swap(&pq->pqArr[largest], &pq->pqArr[index]); 
+        heapify(pq, size, largest); 
+    }
+
+}
+
+
+void heapsort(PriorityQueue * pq){
+    for(int i = pq->size/2 - 1; i >= 0; i--){
+        heapify(pq, pq->size, i); 
+    }
+
+
+    for(int i = pq->size - 1; i >= 0; i--){
+        swap(&(pq->pqArr[0]), &(pq->pqArr[i])); 
+        heapify(pq, i, 0); 
+    }
+}
+
+
+
+void swap(int * a, int * b){
+    int temp = *a; 
+    *a = *b; 
+    *b  = temp; 
+}
+
+
+void printHeap(PriorityQueue * pq){
+    
+    printf("Printing Heap\nHeap: ");
+    for(int i = 0; i < pq->size; i++){
+        printf("%d ", pq->pqArr[i]); 
+    }
+
+    printf("\n\n"); 
+}
